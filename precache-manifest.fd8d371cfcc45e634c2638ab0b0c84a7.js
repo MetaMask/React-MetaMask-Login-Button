451e14dd712697cfebf1433ab361ee95b857ab87
Sneh Koul <snehkoul@Snehs-MacBook-Pro.local>
2019-02-04 13:22:52 -0500

    Updates

---
+++ precache-manifest.fd8d371cfcc45e634c2638ab0b0c84a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f8fbd099e95b7ad53dc",
+    "url": "/Button-MetaMask/static/js/runtime~main.2f8fbd09.js"
+  },
+  {
+    "revision": "dce432e072470471a335",
+    "url": "/Button-MetaMask/static/js/main.dce432e0.chunk.js"
+  },
+  {
+    "revision": "975ed8db3e292e0bcffa",
+    "url": "/Button-MetaMask/static/js/1.975ed8db.chunk.js"
+  },
+  {
+    "revision": "dce432e072470471a335",
+    "url": "/Button-MetaMask/static/css/main.568040ef.chunk.css"
+  },
+  {
+    "revision": "35c6a377c1ae3cd62da974c085bebf38",
+    "url": "/Button-MetaMask/index.html"
+  }
+];
