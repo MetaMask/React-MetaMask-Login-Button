e269e1540657bddd02f06e60c705e03ef33ebad2
Sneh Koul <snehkoul1999@gmail.com>
2019-04-15 16:49:18 -0700

    supports metamask forwader

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import { Fragment } from 'react'
-import { MetaMaskButton } from 'rimble-ui'
-import Web3 from 'web3';
-import MyComponent from './MyComponent'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal'
-var detect = require('detect-browser').detect
+import React, { Component } from "react";
+import { Fragment } from "react";
+import { MetaMaskButton } from "rimble-ui";
+import Web3 from "web3";
+import MyComponent from "./MyComponent";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+var detect = require("detect-browser").detect;
 
 var isMobile;
-
+var path = window.location.href;
 
 class MetaMaskLoginButton extends Component {
-
   constructor(props, context) {
     super(props, context);
 
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
-    this.network = this.network.bind(this)
-    this.mount = this.mount.bind(this)
+    this.network = this.network.bind(this);
+    this.mount = this.mount.bind(this);
 
     this.init = this.init.bind(this);
-
 
     this.state = {
       show: false,
@@ -33,179 +31,138 @@ class MetaMaskLoginButton extends Component {
     };
   }
   componentWillMount() {
-
-    if (typeof web3 !== 'undefined') {
-    
+    if (typeof web3 !== "undefined") {
       this.state.install = false;
-      window.ethereum.on('networkChanged', (accounts) => {
-       
+      window.ethereum.on("networkChanged", accounts => {
         if (accounts === "3") {
           this.setState({
             isLogin: true,
             isDesiredNetwork: false,
             isMetaMask: false,
             isLoginMetaMask: false
-          })
-        }
-        else {
+          });
+        } else {
           this.setState({
             isLogin: false,
             isDesiredNetwork: true,
             isMetaMask: false,
             isLoginMetaMask: false
-          })
-
+          });
         }
-      })
+      });
     } else {
       this.state.install = true;
     }
   }
 
-
   mount() {
-
-
-    if (typeof web3 !== 'undefined') {
-     
-      this.setState({ isMetaMask: false })
-      this.init()
-
+    if (typeof web3 !== "undefined") {
+      this.setState({ isMetaMask: false });
+      this.init();
     } else {
-     
+      const browser = detect();
 
-      const browser = detect()
-
-     
-
-      isMobile = !!detectMobile()
+      isMobile = !!detectMobile();
 
       function detectMobile() {
         return (
-          navigator.userAgent.match(/Android/i)
-          || navigator.userAgent.match(/webOS/i)
-          || navigator.userAgent.match(/iPhone/i)
-          || navigator.userAgent.match(/iPad/i)
-          || navigator.userAgent.match(/iPod/i)
-          || navigator.userAgent.match(/BlackBerry/i)
-          || navigator.userAgent.match(/Windows Phone/i)
-        )
+          navigator.userAgent.match(/Android/i) ||
+          navigator.userAgent.match(/webOS/i) ||
+          navigator.userAgent.match(/iPhone/i) ||
+          navigator.userAgent.match(/iPad/i) ||
+          navigator.userAgent.match(/iPod/i) ||
+          navigator.userAgent.match(/BlackBerry/i) ||
+          navigator.userAgent.match(/Windows Phone/i)
+        );
       }
       if (!isMobile) {
+        window.open("http://fwd.metamask.io/" + "?" + path);
+
         switch (browser.name) {
+          case "firefox":
+            window.open(
+              "https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/",
+              "_blank"
+            );
 
-          case 'firefox':
-            window.open('https://addons.mozilla.org/en-US/firefox/addon/ether-metamask/', '_blank')
+            break;
 
+          case "chrome":
+            window.open(
+              "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en",
+              "_blank"
+            );
+            break;
 
-            break
+          case "opera":
+            window.open(
+              "https://addons.opera.com/en/extensions/details/metamask/",
+              "_blank"
+            );
 
-          case 'chrome':
-            window.open('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en', '_blank')
-
-          case 'opera':
-            window.open('https://addons.opera.com/en/extensions/details/metamask/', '_blank')
-
-            break
-
+            break;
         }
-
       }
 
       this.setState({ isMetaMask: true });
-
-
-
-
-
     }
-
-
-
-
-
   }
 
   async init() {
     try {
-      const accounts = await window.ethereum.enable()
+      const accounts = await window.ethereum.enable();
       this.setState({
         isDesiredNetwork: true,
         isMetaMask: false
-      })
-      this.network()
-
+      });
+      this.network();
     } catch (error) {
-
-      this.setState({ isLoginMetaMask: true, isMetaMask: false })
-
+      this.setState({ isLoginMetaMask: true, isMetaMask: false });
     }
-    window.ethereum.on('accountsChanged', (accounts) => {
-     
+    window.ethereum.on("accountsChanged", accounts => {
       if (accounts.length === 1) {
         this.setState({
           isLoginMetaMask: true,
           isMetaMask: false,
           isDesiredNetwork: false,
           isLogin: false
-        })
-        this.init()
-
-      }
-      else {
+        });
+        this.init();
+      } else {
         this.setState({
           isLoginMetaMask: false,
           isMetaMask: false,
           isDesiredNetwork: false,
           isLogin: true
-        })
-
-
+        });
       }
-
-
-    })
-
-
+    });
   }
 
   network() {
-    
-
     // If a web3 instance is already provided by Meta Mask.
     if (window.ethereum.networkVersion === "3") {
-      this.setState({ isLogin: true })
+      this.setState({ isLogin: true });
     } else {
-
-      
-      window.ethereum.on('networkChanged', (accounts) => {
-        
+      window.ethereum.on("networkChanged", accounts => {
         if (accounts === "3") {
           this.setState({
             isLogin: true,
             isDesiredNetwork: false,
             isMetaMask: false,
             isLoginMetaMask: false
-          })
-        }
-        else {
+          });
+        } else {
           this.setState({
             isLogin: false,
             isDesiredNetwork: true,
             isMetaMask: false,
             isLoginMetaMask: false
-          })
-
+          });
         }
-      })
-
-
-
+      });
     }
-
   }
-
-
 
   handleClose() {
     window.location.reload();
@@ -214,42 +171,32 @@ class MetaMaskLoginButton extends Component {
 
   handleShow() {
     this.setState({ show: true });
-    this.mount()
+    this.mount();
   }
 
   render() {
     let content;
     if (this.state.isMetaMask) {
-      content = <MyComponent tag='a'></MyComponent>
-
+      content = <MyComponent tag="a" />;
     }
     if (this.state.isLoginMetaMask) {
-      content = <MyComponent tag='b'>
-      </MyComponent>
-
+      content = <MyComponent tag="b" />;
     }
     if (this.state.isDesiredNetwork) {
-      content = <MyComponent tag='c'>
-
-      </MyComponent>
+      content = <MyComponent tag="c" />;
     }
     if (this.state.isLogin) {
-      content = <MyComponent tag='d' ></MyComponent>
+      content = <MyComponent tag="d" />;
     }
     if (isMobile) {
-      content = <Fragment>
-        Mobile coming soon!
-      </Fragment>
+      content = <Fragment>Mobile coming soon!</Fragment>;
     }
-   
 
     return (
-        <div>
-
-        
-
-        <MetaMaskButton mb={3} onClick={this.handleShow}>{this.state.install ? "Install MetaMask" : "Connect with MetaMask"}</MetaMaskButton>
-
+      <div>
+        <MetaMaskButton mb={3} onClick={this.handleShow}>
+          {this.state.install ? "Install MetaMask" : "Connect with MetaMask"}
+        </MetaMaskButton>
 
         <Modal show={this.state.show} onHide={this.handleClose}>
           <Modal.Header closeButton>
@@ -257,7 +204,6 @@ class MetaMaskLoginButton extends Component {
           </Modal.Header>
           <Modal.Body>{content}</Modal.Body>
           <Modal.Footer>
-
             <Button variant="secondary" onClick={this.handleClose}>
               Close
             </Button>
@@ -266,10 +212,9 @@ class MetaMaskLoginButton extends Component {
             </Button>
           </Modal.Footer>
         </Modal>
-        </div>
+      </div>
     );
   }
 }
 
 export default MetaMaskLoginButton;
-
